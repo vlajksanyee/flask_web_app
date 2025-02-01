ec0d173e0f218c7a69d6aac340165fe58053a0d5
vlajksanyee <vlajksanyi@gmail.com>
2025-02-01 06:04:22 +0100

    feat: make web app runnable from script

--- flask_blog.py
+++ flask_blog.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Home Page</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
