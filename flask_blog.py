db3a8cc5aa701e3f04f9838f8b7277d5a1c1caa2
vlajksanyee <vlajksanyi@gmail.com>
2025-02-01 06:07:24 +0100

    feat: add about page

--- flask_blog.py
+++ flask_blog.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Home Page</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
