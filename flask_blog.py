2b3a7c8a109f998f8fc87a818a546fbcd7ea62ec
vlajksanyee <vlajksanyi@gmail.com>
2025-02-03 06:37:12 +0100

    feat(account): create register and login pages

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, url_for
+from flask import flash, Flask, redirect, render_template, url_for
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -30,14 +30,23 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful! Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
