04fd7d6578608c180cd97216bc715c035e0f6703
vlajksanyee <vlajksanyi@gmail.com>
2025-02-04 06:00:22 +0100

    feat(database): create Post model

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 from flask import flash, Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
@@ -14,9 +15,21 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(datetime.timezone.utc))
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
+
 
 test_posts = [
     {
