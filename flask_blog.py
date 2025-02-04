7a82a6f633516efa6ea0e5073a1810acd0f3e5d3
vlajksanyee <vlajksanyi@gmail.com>
2025-02-04 05:48:44 +0100

    feat(database): create User model

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,22 @@
 
 from flask import flash, Flask, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'cbc57430ab3776309a656ca0c6db92eb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image}')"
 
 test_posts = [
     {
