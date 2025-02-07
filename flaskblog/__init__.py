9c2babd4cdc34cd276d04dd8432d4ab0566199ba
vlajksanyee <vlajksanyi@gmail.com>
2025-02-07 05:49:26 +0100

    feat(account): add new users to db
    use Bcrypt to hash passwords

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cbc57430ab3776309a656ca0c6db92eb'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes # import is here to prevent circular imports
 
@@ -13,3 +15,9 @@ from flaskblog import routes # import is here to prevent circular imports
     # from project import app, db
     # app.app_context().push()
     # db.create_all()
+
+
+    # from <app_name> import app, db
+    # app.app_context().push()
+    # from <app_name>.models import User
+    # user = User.query.first()
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import flash, redirect, render_template, url_for
-from flaskblog import app
+from flaskblog import app, bcrypt, db
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
@@ -31,8 +31,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}', 'success')
-        return redirect(url_for('home'))
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created. You are now able to log in.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
