256540a84fcbb0249dc2353fc8e2b4bb3460d5a0
vlajksanyee <vlajksanyi@gmail.com>
2025-02-07 06:02:49 +0100

    feat(account): set up login manager

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,13 @@
 from datetime import datetime, timezone
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -12,6 +18,7 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image}')"
     
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
