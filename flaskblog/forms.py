ab52c9c0cb53831adc06120e193aca8b8dce610d
vlajksanyee <vlajksanyi@gmail.com>
2025-02-09 05:58:34 +0100

    feat(account): implement update profile pic
    validate file extension

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 
 from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
 from flaskblog.models import User
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
@@ -33,6 +34,7 @@ class LoginForm(FlaskForm):
 class UpdateAccount(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,8 @@ from flaskblog import app, bcrypt, db
 from flaskblog.forms import LoginForm, RegistrationForm, UpdateAccount
 from flaskblog.models import User, Post
 from flask_login import current_user, login_required, login_user, logout_user
+import os
+import secrets
 
 test_posts = [
     {
@@ -62,11 +64,22 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, file_ext = os.path.splitext(form_picture.filename)
+    picture_file_name = random_hex + file_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_file_name)
+    form_picture.save(picture_path)
+    return picture_file_name
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccount()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
