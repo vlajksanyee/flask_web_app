b21f72e58b16504f8a0dd4562c5f1555949b301e
vlajksanyee <vlajksanyi@gmail.com>
2025-02-12 05:55:07 +0100

    feat(account): add methods to get and verify reset token
    For resetting email or password

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
-from flaskblog import db, login_manager
+from flaskblog import app, db, login_manager
 from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,6 +15,19 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
